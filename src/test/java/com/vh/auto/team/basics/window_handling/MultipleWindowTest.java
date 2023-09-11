80d36ef344a7f6bb9cb85b402bee3955f177213d
youvegotnigel <nigeldave19@gmail.com>
2023-09-11 22:19:26 +0530

    Update MultipleWindowTest.java

--- src/test/java/com/vh/auto/team/basics/window_handling/MultipleWindowTest.java
+++ src/test/java/com/vh/auto/team/basics/window_handling/MultipleWindowTest.java
@@ -1,6 +1,5 @@
 package com.vh.auto.team.basics.window_handling;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
