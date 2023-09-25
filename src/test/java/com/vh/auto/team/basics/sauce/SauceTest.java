fe1b40543c9371ffff4f77fcdf0682aacf1bb298
youvegotnigel <nigeldave19@gmail.com>
2023-09-25 17:18:25 +0530

    Update SauceTest.java

--- src/test/java/com/vh/auto/team/basics/sauce/SauceTest.java
+++ src/test/java/com/vh/auto/team/basics/sauce/SauceTest.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
