7444997532691e206cd337e8e8bf286dffa33df5
youvegotnigel <nigeldave19@gmail.com>
2023-10-22 10:49:33 +0530

    Create ProxyTest.java

---
+++ src/test/java/com/vh/auto/team/basics/selenium_manager/ProxyTest.java
@@ -0,0 +1,58 @@
+package com.vh.auto.team.basics.selenium_manager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class ProxyTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void noProxyTest() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("stable");
+        driver = new ChromeDriver(chromeOptions);
+        driver.get("https://mylocation.org/");
+        driver.manage().window().maximize();
+        String ipinfo = findElement(By.xpath("(//table)[1]")).getText().trim();
+        System.out.println("\n" + ipinfo);
+        driver.quit();
+        System.out.println("-----------------------------------------------------------------------------------\n");
+    }
+
+    @Test
+    public void proxyTest() {
+
+        Proxy proxy = new Proxy();
+        //proxy.setAutodetect(false);
+        //proxy.setHttpProxy("34.82.224.175:33333");
+        proxy.setSslProxy("34.82.224.175:33333");
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setBrowserVersion("stable");
+        chromeOptions.setCapability("proxy", proxy);
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.get("https://mylocation.org/");
+        driver.manage().window().maximize();
+        String ipinfo = findElement(By.xpath("(//table)[1]")).getText().trim();
+        System.out.println("\n" + ipinfo);
+        driver.quit();
+        System.out.println("-----------------------------------------------------------------------------------\n");
+    }
+
+
+    public WebElement findElement(By element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(element));
+    }
+}
