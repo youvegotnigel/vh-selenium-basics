a2e1778fbdd0d49a2cfe1562b601487c7b26914c
youvegotnigel <nigeldave19@gmail.com>
2024-06-26 13:54:04 +0530

    Update OWASP_ZAP_Test.java

--- src/test/java/com/vh/auto/team/basics/owasp/OWASP_ZAP_Test.java
+++ src/test/java/com/vh/auto/team/basics/owasp/OWASP_ZAP_Test.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.zaproxy.clientapi.core.ApiResponse;
 import org.zaproxy.clientapi.core.ClientApi;
 import org.zaproxy.clientapi.core.ClientApiException;
@@ -20,13 +18,14 @@ public class OWASP_ZAP_Test {
 
     static final String ZAP_PROXY_ADDRESS = "localhost";
     static final int ZAP_PROXY_PORT = 1010;
-    static final String ZAP_API_KEY = "";
+    static final String ZAP_API_KEY = "2esc81e5kaalo5n7g8dntva1r1";
+    private static final String ZAP_FILE_PATH = "C:\\Program Files\\OWASP\\Zed Attack Proxy";
     WebDriver driver;
     ClientApi api;
 
 
-    @BeforeTest
-    public void setup() {
+    @BeforeMethod
+    public void setup() throws ClientApiException {
         String proxyStr = ZAP_PROXY_ADDRESS + ":" + ZAP_PROXY_PORT;
         Assert.assertTrue(isOnline("http://" + proxyStr));
 
@@ -42,33 +41,65 @@ public class OWASP_ZAP_Test {
         driver = new ChromeDriver(chromeOptions);
 
         api = new ClientApi(ZAP_PROXY_ADDRESS, ZAP_PROXY_PORT, ZAP_API_KEY);
-
     }
 
 
     @AfterMethod
     public void tearDown() throws ClientApiException {
-        if (api != null) {
-            String title = "My ZAP report";
-            String template = "modern";
-            String description = "This is a sample report";
-            String reportFileName = "zap-modern-report.html";
-            String targetFolder = new File("").getAbsolutePath();
-            ApiResponse response = api.reports.generate(title, template, null,
-                    description, null, null, null, null, null, reportFileName,
-                    null, targetFolder, null);
-            System.out.println("ZAP report generated at: " + response.toString());
-        }
         if (driver != null) {
             driver.quit();
         }
+        //api.core.shutdown();
+        //api.core.deleteSiteNode();
+    }
+
+    @Test
+    public void scanJuiceShop() {
+        driver.get("https://juice-shop.herokuapp.com/#/");
+        Assert.assertTrue(driver.getTitle().contains("Juice"));
+        generateZAPReport("This is to scan Juice SHOP site", "juice-shop-scan");
     }
 
 
     @Test
-    public void testSecurity() {
+    public void scanDialog() {
         driver.get("https://www.dialog.lk/");
         Assert.assertTrue(driver.getTitle().contains("Dialog"));
+        generateZAPReport("This is to scan dialog site", "dialog-scan");
+    }
+
+    @Test
+    public void scanSLT() {
+        driver.get("https://www.slt.lk/");
+        Assert.assertTrue(driver.getTitle().contains("SLTMobitel"));
+        generateZAPReport("This is to scan SLT site", "slt-scan");
+    }
+
+    @Test
+    public void scanRooter() {
+        driver.get("https://rooter.lk/");
+        Assert.assertTrue(driver.getTitle().contains("Apple"));
+        generateZAPReport("This is to scan rooter site", "rooter-scan");
+    }
+
+    private void generateZAPReport(String description, String fileName){
+        if (api != null) {
+            String title = "My ZAP report";
+            String template = "high-level-report";
+            //String description = "This is a sample report";
+            String reportFileName = String.format("%s-zap-report.html", fileName);
+            String targetFolder = new File("").getAbsolutePath();
+            ApiResponse response = null;
+            try {
+                response = api.reports.generate(title, template, null,
+                        description, null, null, null, null, null, reportFileName,
+                        null, targetFolder, null);
+            } catch (ClientApiException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("ZAP report generated at: " + response.toString());
+
+        }
     }
 
 
